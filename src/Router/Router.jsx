0190e4fd6268b7b3e30cff91e40b4b23fbb119cd
Shatak-the-imposter <helloshatak@gmail.com>
2024-08-14 21:34:02 +0600

    project created ,tailwind, daisyUI , react router setup done

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-5xl text-red-500">Hello world!</div>,
+    },
+  ]);
+  
+
+export default Router;
